d7910e1f2ab553589d5b5406ef7e1e396f3958ee
sri <sriramrmk@gmail.com>
2024-08-10 11:22:13 -0400

    adding comments in scripts for easy understanding

--- redis.sh
+++ redis.sh
@@ -1,4 +1,8 @@
-source common.sh #before calling a function, it has to be sourced., here it is being sourced from common.sh
+source common.sh #before calling a function, it has to be sourced. here it is being sourced from common.sh.
+# information is inherited from common.sh and while execution of this script, information from common.sh is substituted (made available).
+# this is not the same as the file being executed.
+# to execute, you can use bash <filename.sh>
+#if the common.sh exists in another directory, provide the absolute pathname of the file
 component=redis # the component variable is defined individually for each service and is being called in the common.sh file
 
 PRINT Disbale redis default
