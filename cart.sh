a9e064e84626a74c878250c55edc8589a6a12cce
sri <sriramrmk@gmail.com>
2024-08-10 11:11:45 -0400

    adding more info for easy understanding

--- cart.sh
+++ cart.sh
@@ -1,4 +1,5 @@
-source common.sh #before calling a function, it has to be sourced., here it is being sourced from common.sh
+source common.sh #before calling a function, it has to be sourced. here it is being sourced from common.sh. information is inherited from common.sh and while execution of this script, information from common.sh is substituted (made available)
+#if the common.sh exists in another directory, provide the absolute pathname of the file
 component=cart # the component variable is defined individually for each service and is being called in the common.sh file
 app_path=/app
 
