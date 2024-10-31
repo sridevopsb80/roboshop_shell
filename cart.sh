54c64e028ae9fcd7789d0b99057278c93d60c9eb
srikvp <93551165+srikvp@users.noreply.github.com>
2024-10-31 09:37:36 +0530

    updated comments

--- cart.sh
+++ cart.sh
@@ -1,5 +1,5 @@
 #before calling a function, it has to be sourced. here it is being sourced from common.sh.
-# information is inherited from common.sh and while execution of this script, information from common.sh is substituted (made available).
+# information is inherited from common.sh and while executing this script, information from common.sh is substituted (made available).
 # this is not the same as the file being executed.
 # to execute, you can use bash <filename.sh>
 #if the common.sh exists in another directory, provide the absolute pathname of the file
