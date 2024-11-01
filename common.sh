2cd3a2a2a35820898bc029ddb0183c29961b22e8
srikvp <93551165+srikvp@users.noreply.github.com>
2024-11-01 10:06:06 +0530

    updated comments

--- common.sh
+++ common.sh
@@ -9,6 +9,7 @@ PRINT() {
   echo $*
 }
 
+#STAT for status, just a function name to print success and failure messages based on the output of $? command. in case of failure, exit command is used to stop the script execution.
 STAT() {
   if [ $1 -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
